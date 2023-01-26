90ea67f7c3ea902ecf9c24ea7232943610b29a23
Hubbard <ehubbard@una.edu>
2023-01-26 16:42:34 -0600

    Thursday update

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.set('view engine', 'ejs');
 
 let myName = 'Ethan';
 
+app.get('/', async (req,res) => {
+
+
+
+    let result = await res.send(`<h3> Hi, ${ myName } </h3>`)
+
+    console.log( myName );
+})
 
 app.get('/', function (req, res) {
 //  res.send('<!DOCYPE html><html lang ="en"><h1>Hello World, is it November?</h1>')
@@ -23,12 +31,27 @@ app.get('/show', (req, res) => {
 
 })
 
+
 app.get('/ejs', (req,res) => {
 
-    res.render('index');
+    console.log("in /ejs before render:", myName)
+
+    res.render('index', { myName: myName });
+    console.log("after res render /ejs:", myName);
 })
+
+
 console.log('in the node console')
+
 
 app.listen(PORT, () => {
     console.log('Example app running.')
+})
+
+app.get('/name', (req, res) => {
+
+    console.log("in get to slash name:", req.query.ejsFormName);
+
+    res.sendFile('index.html' , { root : __dirname});
+
 })
